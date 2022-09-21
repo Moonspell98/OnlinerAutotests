f5021611fc5289a1a7b5f227fee4e0c041f7cf39
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-09-21 10:14:21 +0200

    Initial commit

---
+++ OnlinerAutotests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OnlinerAutotests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
