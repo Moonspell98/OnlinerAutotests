3a2e777e8b49d8b1db41d89b79b26a3a98d6a58b
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-09-22 16:46:22 +0200

    BasePage.cs class added

---
+++ OnlinerAutotests/Pages/BasePage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+
+namespace OnlinerAutotests.Pages;
+
+public abstract class BasePage
+{
+    private const int WaitForPageLoadingTime = 10;
+    [ThreadStatic] private static IWebDriver _driver;
+
+    protected abstract void OpenPage();
+
+    protected abstract bool IsPageOpened();
+
+    protected BasePage(IWebDriver driver, bool openPageByUrl)
+    {
+        Driver = driver;
+        if (openPageByUrl)
+        {
+            OpenPage();
+        }
+    }
+
+    private void WaitForOpen()
+    {
+        var secondsCount = 0;
+        var isPageOpenedIndicator = IsPageOpened();
+
+        while (!isPageOpenedIndicator && secondsCount < (WaitForPageLoadingTime))
+        {
+            secondsCount++;
+            isPageOpenedIndicator = IsPageOpened();
+        }
+
+        if (!isPageOpenedIndicator)
+        {
+            throw new AssertionException("Page was not opened");
+        }
+    }
+
+    public static IWebDriver Driver
+    {
+        get => _driver;
+        set => _driver = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
